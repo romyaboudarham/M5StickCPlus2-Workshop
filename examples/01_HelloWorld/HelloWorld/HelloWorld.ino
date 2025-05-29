f3351e83243d12371869528f47096fcad5b99212
Romy Aboudarham <63166855+romyaboudarham@users.noreply.github.com>
2025-05-29 13:41:37 -0700

    Update HelloWorld.ino

--- examples/01_HelloWorld/HelloWorld/HelloWorld.ino
+++ examples/01_HelloWorld/HelloWorld/HelloWorld.ino
@@ -1,8 +1,10 @@
-#include <M5StickCPlus2.h> // library
+#include <M5StickCPlus2.h> // this 'include' line includes the M5StickCPlus2 library
+						               // this allows us to use functions written for this specific device
 
-// and "int" is an "integer" which is a WHOLE number (not a fraction) i.e. 1, 6, 17, 394
-int screenWidth = 0;
-int screenHeight = 0;
+// define global variables here
+// an "int" is an "integer" which is a WHOLE number (not a fraction or number with a decimal point) i.e. 1, 6, 17, 394
+int screenWidth = 0;  // this variable is used to set the screen width
+int screenHeight = 0; // this variable is used to set the screen height
 
 void setup() {
   // initialize Serial Monitor to use baud rate 115200
@@ -11,10 +13,15 @@ void setup() {
   auto cfg = M5.config();
   StickCP2.begin(cfg);
 
-  // 0: portrait, 1: landscape, 2: portrait-flipped, 3: landscape-flipped
-  StickCP2.Display.setRotation(1);
+  StickCP2.Display.setRotation(1); // this function sets the rotation of the display
+  								                 // 0: portrait, 1: landscape, 2: portrait-flipped, 3: landscape-flipped
+  
   screenWidth = StickCP2.Display.width();
   screenHeight = StickCP2.Display.height();
+}
+
+void loop() {
+  StickCP2.update();
 
   /*** CUSTOMIZE BEGIN ***/
   // font options: https://github.com/lovyan03/LovyanGFX/blob/55a0f66d9278faa596c8d51a8e8a3e537dd8f44f/src/lgfx/v1/lgfx_fonts.hpp#L329
@@ -23,13 +30,9 @@ void setup() {
   StickCP2.Display.setTextColor(GREEN);
   StickCP2.Display.setTextSize(1); // try 2!
   // text reference point options: https://github.com/lovyan03/LovyanGFX/blob/55a0f66d9278faa596c8d51a8e8a3e537dd8f44f/src/lgfx/v1/misc/enum.hpp#L135
-  StickCP2.Display.setTextDatum(middle_center);
-  StickCP2.Display.drawString("Hello World!", screenWidth/2, screenHeight/2);
+  StickCP2.Display.setTextDatum(top_left);
+  StickCP2.Display.drawString("Hello World!", 0, 0); // prints to the device screen
+  
+  Serial.println("Hello World!"); // prints to the serial monitor
   /*** CUSTOMIZE END ***/
-}
-
-void loop() {
-  StickCP2.update();
-
-  Serial.println("Printing in Serial Monitor!");
 }
